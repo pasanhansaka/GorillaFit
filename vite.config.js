d8cb1863b0563b06efa2b1a0889668cf3260bf2a
Pasan Hansaka <pasanhansaka31@gmail.com>
2026-03-18 11:44:01 +0530

    update site

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   base: '/Gorilla-Fitness/'
